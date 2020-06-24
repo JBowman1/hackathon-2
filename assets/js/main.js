d08546bd5d7a068a1a81acc9606e7e4443a4711d
Jenna Bowman <jbowman1@swarthmore.edu>
2020-06-24 05:27:24 +0000

    Completed api get request.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,23 @@
-const sound = new Sound();
-const options = new Options();
-const app = new App(sound, options);
-app.initializeApp();
+// const sound = new Sound();
+// const options = new Options();
+// const app = new App(sound, options);
+// app.initializeApp();
 
-const hearthstone = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/info",
-  "method": "GET",
-  "headers": {
+$.ajax({
+  async: true,
+  crossDomain: true,
+  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards',
+  type: 'GET',
+  dataType: 'json',
+  headers: {
     "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
     "x-rapidapi-key": "f38359af79mshb1d561b3ec9a230p1a4c57jsn64f7f72fef8b"
+  },
+  success: function(){
+    console.log('success');
+  },
+  error: function () {
+    console.log("error");
   }
-}
-
-$.ajax(hearthstone).done(function(response) {
-  console.log(response);
 });
 
-// const freeSound = {
-//   "url": "https://freesound...",
-//   "method": "GET",
-//   "headers": {
-//     "access_token": "nKsvvepujcVmKQMILlGrkQHirvmTFcP6rrzs11ew"
-//   }
-// }
--- assets/js/preload.js
+++ assets/js/preload.js
@@ -34,3 +34,26 @@ preloadImages([
   "../images/Warlock.png",
   "../images/Warrior.png"
 ]);
+
+const hearthstone = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/%7Bname%7D",
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    "x-rapidapi-key": "f38359af79mshb1d561b3ec9a230p1a4c57jsn64f7f72fef8b"
+  }
+}
+
+$.ajax(hearthstone).done(function (response) {
+  console.log(response);
+});
+
+// const freeSound = {
+//   "url": "https://freesound...",
+//   "method": "GET",
+//   "headers": {
+//     "access_token": "nKsvvepujcVmKQMILlGrkQHirvmTFcP6rrzs11ew"
+//   }
+// }
