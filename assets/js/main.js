226762b0c1b8bf76bb2ea263dc12984780080e97
Jenna Bowman <jbowman1@swarthmore.edu>
2020-06-24 19:53:37 +0000

    Finally made array work.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,39 +3,3 @@
 // const app = new App(sound, options);
 // app.initializeApp();
 
-function handleData(responseData){
-  var names = [];
-  for(var set in responseData) {
-    for (var i = 0; i < responseData[set].length; i++){
-      names.push(responseData[set][i]."name");
-    }
-  }
-  console.log(names);
-}
-
-function getCardData(){
-const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards";
-fetch(url, {
-  async: true,
-  crossDomain: true,
-  method: "GET",
-  withCredentials: true,
-  dataType: 'json',
-  headers: {
-    "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
-    "x-rapidapi-key": "f38359af79mshb1d561b3ec9a230p1a4c57jsn64f7f72fef8b"
-  }
-})
-  .then(resp => resp.json())
-  .then(function (data) {
-    console.log('Success');
-    handleData(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
-$(document).ready(function() {
-  getCardData()
-})
