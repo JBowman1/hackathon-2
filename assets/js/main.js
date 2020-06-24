3758340735b53f10e2eafbf6479b2efe2f223677
Jenna Bowman <jbowman1@swarthmore.edu>
2020-06-24 09:16:04 +0000

    crying.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,39 @@
 // const app = new App(sound, options);
 // app.initializeApp();
 
-$.ajax({
+function handleData(responseData){
+  var names = [];
+  for(var set in responseData) {
+    for (var i = 0; i < responseData[set].length; i++){
+      names.push(responseData[set][i]."name");
+    }
+  }
+  console.log(names);
+}
+
+function getCardData(){
+const url = "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards";
+fetch(url, {
   async: true,
   crossDomain: true,
-  url: 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards',
-  type: 'GET',
+  method: "GET",
+  withCredentials: true,
   dataType: 'json',
   headers: {
     "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com",
     "x-rapidapi-key": "f38359af79mshb1d561b3ec9a230p1a4c57jsn64f7f72fef8b"
-  },
-  success: function(){
-    console.log('success');
-  },
-  error: function () {
-    console.log("error");
   }
-});
+})
+  .then(resp => resp.json())
+  .then(function (data) {
+    console.log('Success');
+    handleData(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
 
+$(document).ready(function() {
+  getCardData()
+})
